9bc78f35584110137981d6e9a92c812f0856386a
panda <ricky_lai@ymail.com>
2020-02-06 18:50:51 -0600

    FB

--- controllers/index.js
+++ controllers/index.js
@@ -4,18 +4,18 @@
 // dependencies
 // =============================================================
 const express = require('express'),
-      router = express.Router(),
-      Article = require('../models/article');
+    router = express.Router(),
+    Article = require('../models/article');
 
 // root route
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     Article
         .find({})
         .where('saved').equals(false)
         .where('deleted').equals(false)
         .sort('-date')
         .limit(20)
-        .exec(function(error, articles) {
+        .exec(function (error, articles) {
             if (error) {
                 console.log(error);
                 res.status(500);
@@ -23,7 +23,7 @@ router.get('/', function(req, res) {
                 console.log(articles);
                 let hbsObj = {
                     title: 'All the News That\'s Fit to Scrape',
-                    subtitle: 'Hacker News Edition',
+                    subtitle: 'Hacker Edition',
                     articles: articles
                 };
                 res.render('index', hbsObj);
@@ -32,14 +32,14 @@ router.get('/', function(req, res) {
 });
 
 // saved articles
-router.get('/saved', function(req, res) {
+router.get('/saved', function (req, res) {
     Article
         .find({})
         .where('saved').equals(true)
         .where('deleted').equals(false)
         .populate('notes')
         .sort('-date')
-        .exec(function(error, articles) {
+        .exec(function (error, articles) {
             if (error) {
                 console.log(error);
                 res.status(500);
@@ -47,10 +47,10 @@ router.get('/saved', function(req, res) {
                 console.log(articles);
                 let hbsObj = {
                     title: 'All the News That\'s Fit to Scrape',
-                    subtitle: 'Saved Hacker News',
+                    subtitle: 'Saved News',
                     articles: articles
                 };
-                res.render("saved" ,hbsObj);
+                res.render("saved", hbsObj);
             }
         });
 });
