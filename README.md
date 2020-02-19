e9bca60df8b736b23680d7cec14c21b495ee86f8
panda <ricky_lai@ymail.com>
2020-02-19 17:10:29 -0600

    feel better

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# WebScrape
+# Hacker News Scrapper
+###### "All the News That's Fit to Scrape" Assignment | Week 18 Upenn Coding Bootcamp
+
+## Tehcnologies Used
+  * express
+  * express-handlebars
+  * mongoose
+  * body-parser
+  * cheerio
+  * request
+  * morgan
+  
+ ## Overview
+ 
+Hacker News Scrapper is a simple , easily navigatable site that allows user to scrape current news article from Hacker News. The site uses cheerio to scrame news and then store the data in mongoDb. Users can save , leave notes and delete articles based on their preference. 
+* To view a live version of the site please visit https://hacker-news-scrapper.herokuapp.com/ .
+
+![hackernewssc](https://user-images.githubusercontent.com/36983507/47628254-0b916000-db0b-11e8-8c98-2b60467e444c.JPG)
--- controllers/api/notes.js
+++ controllers/api/notes.js
@@ -3,17 +3,17 @@
 // dependencies
 // =============================================================
 const express = require('express'),
-      router = express.Router(),
-      request = require('request'),
-      cheerio = require('cheerio'),
-      Article = require('../../models/article'),
-      Note = require('../../models/note');
+    router = express.Router(),
+    request = require('request'),
+    cheerio = require('cheerio'),
+    Article = require('../../models/article'),
+    Note = require('../../models/note');
 
 // get all notes
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     Note
         .find({})
-        .exec(function(err, notes) {
+        .exec(function (err, notes) {
             if (err) {
                 console.log(err);
                 res.status(500);
@@ -24,9 +24,9 @@ router.get('/', function(req, res) {
 });
 
 // add a note to a saved article
-router.post('/:id', function(req, res) {
+router.post('/:id', function (req, res) {
     let newNote = new Note(req.body);
-    newNote.save(function(err, doc) {
+    newNote.save(function (err, doc) {
         if (err) {
             console.log(err);
             res.status(500);
@@ -34,7 +34,7 @@ router.post('/:id', function(req, res) {
             Article.findOneAndUpdate(
                 { _id: req.params.id },
                 { $push: { 'notes': doc.id } },
-                function(error, newDoc) {
+                function (error, newDoc) {
                     if (error) {
                         console.log(error);
                         res.status(500);
@@ -48,8 +48,8 @@ router.post('/:id', function(req, res) {
 });
 
 // delete a note from a saved article
-router.delete('/:id', function(req, res) {
-    Note.findByIdAndRemove(req.params.id, function(err, note) {
+router.delete('/:id', function (req, res) {
+    Note.findByIdAndRemove(req.params.id, function (err, note) {
         if (err) {
             console.log(err);
             res.status(500);
